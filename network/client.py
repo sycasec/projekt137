3532eaf56f1906196b783f23d58aae893c3d7412
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-11-19 00:02:53 +0800

    allow caller to handle client sending
    Previously, myClient would require input from user to send messages
    Now, caller of myClient() just needs to call myClient.sender(data)

--- network/client.py
+++ network/client.py
@@ -12,9 +12,6 @@ class myClient:
         t_receive = threading.Thread(target=self.broadcast_receiver)
         t_receive.start()
 
-        t_send = threading.Thread(target=self.sender)
-        t_send.start()
-
     def broadcast_receiver(self):
         while True:
             s_msg_bin = self.s.recv(1024)
@@ -27,18 +24,12 @@ class myClient:
             print("Broadcast message received: ", s_msg)       
 
 
-    def sender(self, data=None, data_is_string=False):
-        if data is None:
-            data = {"I am": "a dummy placeholder"}
-
-        while True:
-            if data_is_string:
-                data = input("Input message to send: ")
-                to_send = data.encode()
-            else:
-                input("Press enter to send data")
-                to_send = pickle.dumps(data)
-            self.s.send(to_send)
+    def send(self, data):
+        try:
+            to_send = data.encode()
+        except AttributeError:
+            to_send = pickle.dumps(data)
+        self.s.send(to_send)
 
 if __name__=="__main__":
     myClient()
