fe06ba52a4e88f87093c435f934b967242b321ff
sycasec <kadelcastillo@up.edu.ph>
2023-11-22 04:06:40 +0800

    color changing keys

--- keys.py
+++ keys.py
@@ -6,16 +6,18 @@ class Key:
     key_red_color = pygame.Color(224,102,102) 
     key_green_color = pygame.Color(147,196,125) 
     key_default_color = pygame.Color(199,199,199)
-    key_rect_size = 50
+    key_rect_size = 60
+    b_rad = 3 
 
-
-    def __init__(self, assigned_key, coords):
+    def __init__(self, assigned_key, coords, t_font, key_code):
         self.key = assigned_key
         self.x = coords[0]
         self.y = coords[1] 
         self.color = self.key_default_color
         self.target_color = self.key_default_color
         self.t_timer = 0
+        self.key_code = key_code
+        self.font = t_font
 
     def toggle_red(self):
         self.target_color = self.key_red_color
@@ -39,12 +41,68 @@ class Key:
         if elapsed_time < self.t_duration:
             interpolation_factor = elapsed_time / self.t_duration 
             self.color = pygame.Color(
-                int(pygame.math.lerp(self.color.r, self.target_color.r, interpolation_factor)),
-                int(pygame.math.lerp(self.color.g, self.target_color.g, interpolation_factor)),
-                int(pygame.math.lerp(self.color.b, self.target_color.b, interpolation_factor))
+                int(pygame.math.lerp(
+                    self.color.r, self.target_color.r, interpolation_factor)),
+                int(pygame.math.lerp(
+                    self.color.g, self.target_color.g, interpolation_factor)),
+                int(pygame.math.lerp(
+                    self.color.b, self.target_color.b, interpolation_factor))
             )
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.key_rect_size, self.key_rect_size))
+
+        # render rounded rect
+        pygame.draw.rect(screen, self.color,
+            pygame.Rect(self.x, self.y, self.key_rect_size, self.key_rect_size), 
+            0,
+            self.b_rad
+        )
+
+        # render inner rectangle
+        pygame.draw.rect(screen, self.color,
+            pygame.Rect(self.x + self.b_rad, 
+             self.y + self.b_rad, 
+             self.key_rect_size - (2 * self.b_rad), 
+             self.key_rect_size - (2 * self.b_rad)) 
+        )
+
+        t_surf = self.font.render(self.key, True, (255,255,255))
+        t_rect = t_surf.get_rect(center=(self.x + self.key_rect_size // 2, self.y + self.key_rect_size//2))
+        screen.blit(t_surf, t_rect)
 
 
+class KeyHelper:
+    def __init__(self, keys_font):
+        self.top_row = "QWERTYUIOP"
+        self.mid_row = "ASDFGHJKL"
+        self.bot_row = "ZXCVBNM"
+        self.y_vals = [420,495,570]
+        self.x_vals = [145,190,265]
+        self.x_inc = 85
+        self.font = keys_font
+        self.keys_dict = {}
+
+    def gen_key_row(self, row):
+        kd = {}
+        sel_row = self.top_row
+
+        if row == 1:
+            sel_row = self.mid_row
+        elif row == 2:
+            sel_row = self.bot_row
+
+        c_x = self.x_vals[row]
+
+        for char in sel_row:
+            key_code = pygame.K_a + (ord(char.upper()) - ord('A'))
+            kd[char] = Key(char, (c_x, self.y_vals[row]), self.font, key_code)
+            c_x += self.x_inc
+
+        return kd
+
+    def gen_keys(self):
+        for i in range(0,3):
+            self.keys_dict.update(self.gen_key_row(i))
+
+    def get_keys(self):
+        return self.keys_dict
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import pygame
 
-from keys import Key
+from keys import Key, KeyHelper
 from background import Background
 
 WINDOW_WIDTH = 1125 
@@ -21,6 +21,7 @@ h1_size: int = 50
 
 # I dont have DM sans installed yet.
 h1_dm_sans = pygame.font.Font("InterNerdFontPropo-Bold.otf", h1_size)
+keys_font = pygame.font.Font("InterNerdFontPropo-Bold.otf",36)
 
 # Temporary Text Surfaces 
 title_surface = h1_dm_sans.render("KEYBOARD SPLATOON", True, "Black")
@@ -30,10 +31,17 @@ bg = Background([WINDOW_WIDTH, WINDOW_HEIGHT]).generate("White")
 
 
 # Test Key Class 
-square_position = (WINDOW_WIDTH // 2 - 150 // 2, 400)
-jkey_pos = (WINDOW_WIDTH // 2 + 150 // 2, 400)
-qkey = Key('Q', square_position)
-jkey = Key('J', jkey_pos)
+# square_position = (160, 420)
+# jkey_pos = (200, 485)
+# qkey = Key('Q', square_position, keys_font)
+# jkey = Key('J', jkey_pos, keys_font)
+
+keys = KeyHelper(keys_font)
+keys.gen_keys()
+k_dict = keys.get_keys()
+# top_row = keys.gen_top_row()
+# top_row = keys.gen_mid_row()
+# bot_row = keys.gen_bot_row()
 
 # main loop
 while True:
@@ -42,23 +50,25 @@ while True:
             pygame.quit()
             exit()
     # --------------------------------- EXPERIMENTAL --------------------------------
-        # not sure how to make it so that we wont have to if else for each key
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
-            # Toggle square color on "q" key press
-            qkey.on_key_press()
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_j:
-            jkey.on_key_press()
-
+        elif event.type == pygame.KEYDOWN:
+            for key in k_dict.values():
+                if event.key == key.key_code:
+                    key.on_key_press()
     # --------------------------------- EXPERIMENTAL --------------------------------
 
-    qkey.update_color()
-    jkey.update_color()
+    # qkey.update_color()
+    # jkey.update_color()
+    for k in k_dict.values():
+        k.update_color()
 
     screen.blit(bg, (0,0))
     screen.blit(title_surface, (290, 139))
 
-    qkey.draw(screen)
-    jkey.draw(screen)
+    # qkey.draw(screen)
+    # jkey.draw(screen)
+
+    for k in k_dict.values():
+        k.draw(screen)
 
     pygame.display.update()
     GAME_CLOCK.tick(60)
