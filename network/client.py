87e39e9807c5eb610b9026fa6985b59ef16f92e7
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-11-22 10:03:12 +0800

    Add `on_receive` function to execute when receiving message

--- network/client.py
+++ network/client.py
@@ -3,10 +3,15 @@ import socket
 import threading
 
 class myClient:    
-    def __init__(self, host="0.0.0.0", port=7634):
+    def __init__(self, 
+                 host="0.0.0.0", 
+                 port=7634, 
+                 on_receive=lambda msg: print(f"Broadcast message received: {msg}")
+                 ):
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.host = host
         self.port = port
+        self.on_receive = on_receive
         self.s.connect((self.host, self.port))
 
         t_receive = threading.Thread(target=self.broadcast_receiver)
@@ -21,7 +26,7 @@ class myClient:
             except UnicodeDecodeError:
                 s_msg = pickle.loads(s_msg_bin)
 
-            print("Broadcast message received: ", s_msg)       
+            self.on_receive(s_msg)
 
 
     def send(self, data):
