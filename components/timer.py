2481d5ea2675b3bc302b58ee3024883fabf82e94
some1els <satupa@up.edu.ph>
2023-11-25 16:00:30 +0800

    add timer graphics

---
+++ components/timer.py
@@ -0,0 +1,34 @@
+import pygame
+
+class Timer:
+    b_rad = 10
+    timer_run_color = pygame.Color(224, 102, 102)
+    timer_bg_color = pygame.Color(217, 217, 217)
+
+    def __init__(self, x, y, width, max_time, font):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = 20
+        self.max_time = max_time
+        self.current_time = max_time
+        self.font = font
+
+    def update(self, dt):
+        self.current_time -= dt
+        if self.current_time < 0:
+            self.current_time = 0
+
+    def reset(self):
+        self.current_time = self.max_time
+
+    def draw(self, surface):
+        # Draw text
+        text_surface = self.font.render("TIMER", True, (0, 0, 0))
+        surface.blit(text_surface, (self.x , self.y))
+
+        # Draw timer bar
+        x = self.x + text_surface.get_width() + 20
+        fill_width = int((self.current_time / self.max_time) * self.width)
+        pygame.draw.rect(surface, self.timer_bg_color, (x, self.y, self.width, self.height), 0, self.b_rad)
+        pygame.draw.rect(surface, self.timer_run_color, (x, self.y, fill_width, self.height), 0, self.b_rad)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame
 from components.keys import KeyHelper
 from components.background import Background
 from network.client import myClient
+from components.timer import Timer
 
 WINDOW_WIDTH = 1125 
 WINDOW_HEIGHT = 800 
@@ -30,6 +31,9 @@ title_surface = h1_dm_sans.render("KEYBOARD SPLATOON", True, "Black")
 # Surfaces
 bg = Background([WINDOW_WIDTH, WINDOW_HEIGHT]).generate("White")
 
+# Timer
+timer_font = pygame.font.Font(pygame.font.get_default_font(), 20)
+timer_bar = Timer(50, 50, (WINDOW_WIDTH-200), 10, timer_font)
 
 # Key generation
 keys = KeyHelper(keys_font)
@@ -68,6 +72,10 @@ while True:
 
     for k in k_dict.values():
         k.draw(screen)
+    
+    # dt = GAME_CLOCK.tick(60) / 1000.0 
+    # timer_bar.update(dt)
+    # timer_bar.draw(screen)
 
     pygame.display.update()
     GAME_CLOCK.tick(60)
