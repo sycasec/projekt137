6c4bb25adac9b998fd17c0b070db981f95929795
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-11-26 00:04:30 +0800

    draw scores in pygame

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 
 from components.keys import KeyHelper
 from components.background import Background
+from components.score import ScoreHelper
 from network.client import myClient
 
 WINDOW_WIDTH = 1125 
@@ -11,7 +12,6 @@ WINDOW_HEIGHT = 800
 FACTOR = 25
 WINDOW_TITLE = "Keyboard Splatoon"
 GAME_CLOCK = pygame.time.Clock()
-SCORES = {"R":0, "G":0}
 
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -20,9 +20,11 @@ pygame.display.set_caption(WINDOW_TITLE)
 
 # Temporary Fonts 
 h1_size: int = 50
+h3_size: int = 35
 
 # I dont have DM sans installed yet.
 h1_dm_sans = pygame.font.Font(pygame.font.get_default_font(), h1_size)
+score_font = pygame.font.Font(pygame.font.get_default_font(), h3_size)
 keys_font = pygame.font.Font(pygame.font.get_default_font(),36)
 
 # Temporary Text Surfaces 
@@ -37,21 +39,27 @@ keys = KeyHelper(keys_font)
 keys.gen_keys()
 k_dict = keys.get_keys()
 
+# Score generation
+scores = ScoreHelper(score_font)
+
 # Network actions
 def receive_keypress(key):
     print(f"Broadcast received: {key}")
     try:
         key_obj = k_dict[key]
-        key_obj.on_key_press()
-        if key_obj.target_color == key_obj.key_green_color:
-            to_add = "G"
-        elif key_obj.target_color == key_obj.key_red_color:
-            to_add = "R"
-        SCORES[to_add] += 10
-
-        print(SCORES)
     except KeyError:
-        pass
+        return
+
+    key_obj.on_key_press()
+    # TODO: If you press one key extremely fast, sometimes one player 
+    # will get multiple points per color toggle. 
+    # Ex. scores are 200-150, even if you just spammed Q there's a slight bias to green
+    # This can be seemingly fixed by reducing Key.t_duration to 100
+    if key_obj.target_color == key_obj.key_green_color:
+        to_add = scores.GREEN
+    elif key_obj.target_color == key_obj.key_red_color:
+        to_add = scores.RED
+    scores.add_score(to_add)
 
 c = myClient(on_receive=receive_keypress)
 
@@ -76,6 +84,7 @@ while True:
     screen.blit(bg, (0,0))
     screen.blit(title_surface, (290, 139))
 
+    scores.draw(screen)
     for k in k_dict.values():
         k.draw(screen)
 
