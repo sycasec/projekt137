785c4ea42cfc156306ee1bf62b44aa843ee4056c
jugu612 <jngudito@up.edu.ph>
2023-11-26 01:56:08 +0800

    modified main.py and added title screen, about page, loading screen, color assignment screen, countdown

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import pygame
 from components.keys import KeyHelper
 from components.background import Background
 from network.client import myClient
+from screens.home import HomeScreen
+from screens.about import AboutScreen
+from screens.loading import Waiting
+from screens.assignment import ColorAssignment
+from screens.countdown import Countdown
 
 WINDOW_WIDTH = 1125 
 WINDOW_HEIGHT = 800 
@@ -45,6 +50,13 @@ def receive_keypress(key):
         pass
 
 c = myClient(on_receive=receive_keypress)
+home_screen = HomeScreen(WINDOW_WIDTH, WINDOW_HEIGHT, keys_font)
+about_screen = AboutScreen(WINDOW_WIDTH, WINDOW_HEIGHT, keys_font)
+waiting_screen = Waiting(WINDOW_WIDTH, WINDOW_HEIGHT, keys_font)
+assignment_screen = ColorAssignment(WINDOW_WIDTH, WINDOW_HEIGHT, keys_font)
+countdown_screen = Countdown(WINDOW_WIDTH, WINDOW_HEIGHT, keys_font)
+active_screen = "home"
+
 
 # main loop
 while True:
@@ -52,22 +64,65 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # --------------------------------- EXPERIMENTAL --------------------------------
         elif event.type == pygame.KEYDOWN:
-            try: 
-                c.send(k_dict[event.key].key)
-            except KeyError as e:
-                print(f"KeyError: {e}")
-    # --------------------------------- EXPERIMENTAL --------------------------------
+            # --------------------------------- EXPERIMENTAL --------------------------------
+                try: 
+                    c.send(k_dict[event.key].key)
+                except KeyError as e:
+                    print(f"KeyError: {e}")
+            # --------------------------------- EXPERIMENTAL --------------------------------        
+        result = home_screen.handle_event(event)
+        if result is not None:
+            if result == 0:  # initialize a game button
+                active_screen = "play"
+            elif result == 1:  # join a game button
+                active_screen = "load"
+            elif result == 2:  # About button
+                active_screen = "about"
+            elif result == 3:  # Quit button
+                pygame.quit()
+                exit()
+        elif active_screen == "about":
+            about_result = about_screen.handle_event(event)
+            if about_result == "back":
+                active_screen = "home"
+        elif active_screen == "assignment":
+            assignment_screen.render(screen)
+            result = assignment_screen.handle_event(event)
+            if result == "countdown":
+                active_screen = "countdown"
+                countdown_screen.start_countdown()
 
-    for k in k_dict.values():
-        k.update_color()
+    if active_screen == "home":
+        home_screen.render(screen)
+        
+    elif active_screen == "play":
+        for k in k_dict.values():
+            k.update_color()
 
-    screen.blit(bg, (0,0))
-    screen.blit(title_surface, (290, 139))
+        screen.blit(bg, (0, 0))
+        screen.blit(title_surface, (290, 139))
 
-    for k in k_dict.values():
-        k.draw(screen)
+        for k in k_dict.values():
+            k.draw(screen)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            active_screen = "home"
+
+    elif active_screen == "about":
+        about_screen.render(screen)
+
+    elif active_screen == "load":
+        waiting_screen.update()
+        waiting_screen.render(screen)
+        result = waiting_screen.handle_event(event)
+        if result == "assignment":
+            active_screen = "assignment"
+            
+    elif active_screen == "countdown":
+        countdown_screen.render(screen)
+        if countdown_screen.is_complete():
+            active_screen = "play"
+
 
     pygame.display.update()
     GAME_CLOCK.tick(60)
---
+++ screens/home.py
@@ -0,0 +1,62 @@
+import pygame
+import sys
+
+class HomeScreen:
+    def __init__(self, screen_width, screen_height, font):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.font = font
+        self.title_font_size = 50
+        self.button_font_size = 24
+        self.button_spacing = 40  
+        self.title_spacing = 50 
+        self.selected_button = 0
+    
+        self.title()
+        self.menu()
+
+    def title(self):
+        self.title_font = pygame.font.Font(pygame.font.get_default_font(), self.title_font_size)  
+        title_text = "KEYBOARD SPLATOON"
+        self.title_surface = self.title_font.render(title_text, True, "Black")
+       
+    def menu(self):
+        self.button_font = pygame.font.Font(pygame.font.get_default_font(), self.button_font_size)
+        self.buttons = ["Initialize game", "Join a game", "About", "Quit"]
+        self.button_surfaces = [self.button_font.render(button, True, "Black") for button in self.buttons]
+        title_rect = self.title_surface.get_rect(center=(self.screen_width // 2, self.screen_height // 2 - self.title_spacing))
+        self.button_rects = [surface.get_rect(topleft=(title_rect.left + 50, title_rect.bottom + i * self.button_spacing + 16)) for i, surface in enumerate(self.button_surfaces)]
+
+    def handle_event(self, event):
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                for i, rect in enumerate(self.button_rects):
+                    if rect.collidepoint(event.pos):
+                        return i
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                self.select_button(1)
+            elif event.key == pygame.K_UP:
+                self.select_button(-1)
+            elif event.key == pygame.K_RETURN: 
+                return self.selected_button
+
+    def select_button(self, dir):
+        self.selected_button = (self.selected_button + dir) % len(self.buttons)
+
+    def render(self, screen):
+        screen.fill((255, 255, 255))
+        screen.blit(self.title_surface, self.title_surface.get_rect(center=(self.screen_width // 2, self.screen_height // 2 - self.title_spacing)))
+
+        for i, (rect, surface) in enumerate(zip(self.button_rects, self.button_surfaces)):
+            # Highlight the selected button
+            if i == self.selected_button:
+                enlarged_rect = rect.inflate(15, 15)
+                pygame.draw.rect(screen, (217, 217, 217), enlarged_rect, border_radius=10)
+
+            screen.blit(surface, rect)
+
+        pygame.display.flip()
