41fbe2319c69e412b2e6ff4f1eab0274842d4271
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-12-06 07:35:48 +0800

    Display local IP on Linux
    code taken from: https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib

--- network/server.py
+++ network/server.py
@@ -26,8 +26,7 @@ class myServer:
         self.serverLoop.start()
         
         # Get host IP address
-        hostname = socket.gethostname()
-        self.hostAddress = socket.gethostbyname(hostname)
+        self.hostAddress = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
 
     def __del__(self):
