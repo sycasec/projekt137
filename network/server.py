e17601fd92e61deb70bc0e38c3d10cac9ce925df
Alwyn Dy <ardy@up.edu.ph>
2023-12-10 21:06:45 +0800

    fix for error code

--- network/server.py
+++ network/server.py
@@ -4,43 +4,62 @@ import pickle
 import time
 import sys
 
-class myServer:
 
+class myServer:
     def __init__(self, host="0.0.0.0", port=7634):
-        self.server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Prevent OSError: [Errno 98] Address already in use
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
+
         self.clientList = []
-        self.host=host
-        self.port=port
+        self.host = host
+        self.port = port
         self.serverRun = True
 
-        #Functions to start the server and define listener
-        self.server.bind((self.host,self.port))
+        # Functions to start the server and define listener
+        self.server.bind((self.host, self.port))
         self.server.listen(1)
 
         self.connected_players = 0
         self.status = "WAIT"
-        
-        #Functions to run the server
+
+        # Functions to run the server
         self.serverLoop = threading.Thread(target=self.mainLoop)
         self.serverLoop.daemon = True
         self.serverLoop.start()
-        
-        
-        # Get host IP address
-        self.hostAddress = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 
-    
+        # Get host IP address
+        self.hostAddress = [
+            l
+            for l in (
+                [
+                    ip
+                    for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+                    if not ip.startswith("127.")
+                ][:1],
+                [
+                    [
+                        (s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close())
+                        for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]
+                    ][0][1]
+                ],
+            )
+            if l
+        ][0][0]
+
     def __del__(self):
         self.server.close()
-        
+
     def kill(self):
         self.serverRun = False
-        
-        self.server.shutdown(socket.SHUT_RDWR)
-        self.server.close()
+
+        if self.server.fileno() > 0:
+            self.server.shutdown(socket.SHUT_RDWR)
+            self.server.close()
+
+        for x in self.clientList:
+            x.close()
+
         self.clientList = []
         self.connected_players = 0
 
@@ -76,26 +95,29 @@ class myServer:
                 c_msg = pickle.loads(c_msg_bin)
             except:
                 pass
-            
+
             print(f"message from {adr}: {c_msg}")
             self.broadcast(c_msg_bin)
 
     def on_client_connect(self, conn, addr):
-        conn.send(f"Welcome, {addr}!".encode())
+        try:
+            conn.send(f"Welcome, {addr}!".encode())
 
-        print("connected with",addr)
+            print("connected with", addr)
 
-        self.connected_players += 1
+            self.connected_players += 1
 
-        t = threading.Thread(target=self.clientHandler, args=(conn,addr))
-        t.start()
+            t = threading.Thread(target=self.clientHandler, args=(conn, addr))
+            t.start()
 
-        self.clientList.append(conn)
+            self.clientList.append(conn)
+        except:
+            self.kill()
 
-    def broadcast(self,message):
+    def broadcast(self, message):
         for conn in self.clientList:
             conn.send(message)
-    
+
 
 if __name__ == "__main__":
     myServer()
